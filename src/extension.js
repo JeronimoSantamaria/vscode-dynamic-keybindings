1e611972269c7a69ea233cf6ed7bda3b17cb501f
Jerónimo Sánchez Santamaría <jeronimo.s.santamaria@gmail.com>
2025-03-20 11:17:46 -0500

    feat: add support for multiple dynamic keybinding profiles

--- src/extension.js
+++ src/extension.js
@@ -1,17 +1,31 @@
 const vscode = require('vscode');
 
 let dynamicKeybindingsEnabled = true;
+let activeProfile = 'profile1';
 
 function activate(context) {
     vscode.commands.executeCommand('setContext', 'dynamicKeybindingsEnabled', dynamicKeybindingsEnabled);
+    vscode.commands.executeCommand('setContext', 'activeProfile', activeProfile);
 
-    let disposable = vscode.commands.registerCommand('dynamic-keybindings.toggle', function () {
+    let toggleDisposable = vscode.commands.registerCommand('dynamic-keybindings.toggle', function () {
         dynamicKeybindingsEnabled = !dynamicKeybindingsEnabled;
         vscode.commands.executeCommand('setContext', 'dynamicKeybindingsEnabled', dynamicKeybindingsEnabled);
         vscode.window.showInformationMessage(`Dynamic Keybindings ${dynamicKeybindingsEnabled ? 'Enabled' : 'Disabled'}`);
     });
 
-    context.subscriptions.push(disposable);
+    let profile1Disposable = vscode.commands.registerCommand('dynamic-keybindings.profile1', function () {
+        activeProfile = 'profile1';
+        vscode.commands.executeCommand('setContext', 'activeProfile', activeProfile);
+        vscode.window.showInformationMessage('Profile 1 Activated');
+    });
+
+    let profile2Disposable = vscode.commands.registerCommand('dynamic-keybindings.profile2', function () {
+        activeProfile = 'profile2';
+        vscode.commands.executeCommand('setContext', 'activeProfile', activeProfile);
+        vscode.window.showInformationMessage('Profile 2 Activated');
+    });
+
+    context.subscriptions.push(toggleDisposable, profile1Disposable, profile2Disposable);
 }
 
 function deactivate() { }
